1d1619b4d8ef7508142950b8690dc58d73e96698
guerot_a <anthony.guerot@gmail.com>
2014-02-13 16:22:21 +0100

    .

--- src/realloc.c
+++ src/realloc.c
@@ -5,7 +5,7 @@
 ** Login   <guerot_a@epitech.net>
 **
 ** Started on  Wed Feb  5 14:28:13 2014 anthony guerot
-** Last update Thu Feb 13 16:14:26 2014 guerot_a
+** Last update Thu Feb 13 16:21:07 2014 guerot_a
 */
 
 #include "malloc.h"
@@ -15,6 +15,7 @@ void*		realloc(void *ptr, size_t size)
   size_t	min_size;
   t_block*	block;
 
+  printf("realloc\n");
   if (ptr == NULL)
     return (malloc(size));
   if (size == 0)
