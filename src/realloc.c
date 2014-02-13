a53f36804d63b9d280700e56d500844bd437c364
guerot_a <anthony.guerot@gmail.com>
2014-02-13 16:19:39 +0100

    .

--- src/realloc.c
+++ src/realloc.c
@@ -5,44 +5,28 @@
 ** Login   <guerot_a@epitech.net>
 **
 ** Started on  Wed Feb  5 14:28:13 2014 anthony guerot
-** Last update Thu Feb 13 15:50:25 2014 guerot_a
+** Last update Thu Feb 13 16:14:26 2014 guerot_a
 */
 
 #include "malloc.h"
 
 void*		realloc(void *ptr, size_t size)
 {
+  size_t	min_size;
   t_block*	block;
-  int		old_size;
 
-  dbg_start++;
-  /* printf("\033[35;01mralloc of size [%10d] for %p\n", (int)size, ptr); */
   if (ptr == NULL)
     return (malloc(size));
   if (size == 0)
     {
-      printf("fuck\n");
       free(ptr);
       return (NULL);
     }
-  block = get_block(ptr);
-  if (block == NULL)
-    {
-      printf("warning: realloc invalid pointer\n");
-      return (NULL);
-    }
-  old_size = block->size;
-  if ((int)size < old_size)
-    old_size = size;
-  if ((block = malloc(size)) == NULL)
-    {
-      /* printf("ERR\n"); */
-      return (NULL);
-    }
-  /* printf("WTF?!\n"); */
-  memcpy(block->data, ptr, old_size);
+  min_size = ((t_block*)ptr)->size;
+  min_size = (min_size > size) ? size : min_size;
   free(ptr);
-  if (dbg_start > DBG_START)
-    dump_block();
-  return (block->data);
+  block = malloc(size);
+  if (block == NULL)
+    return (NULL);
+  return (memmove(block, ptr, min_size));
 }
